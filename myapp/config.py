c82718b43ac7654e4370ba95db10c6a5ec17044a
HIDDEN GHOST <154367458+hiddenghost9737@users.noreply.github.com>
2025-05-15 10:37:47 +0530

    Update config.py

--- myapp/config.py
+++ myapp/config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
+    SECRET_KEY = os.environ.get('9151409360a2e1974325a87a9dd7473d') or 'your_secret_key_here'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
